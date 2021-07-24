9d08105952fbe3276911cf516baed9e09760cfa6
su'N'mer <6932kenzooii@gmail.com>
2021-07-25 00:04:36 +0800

    Initialized a Hello World WPF view.

---
+++ TemplateWpfApp/Model/MainWindowModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateWpfApp
+{
+    public class MainWindowModel : INotifyPropertyChanged
+    {
+        #region " Properties "
+
+        public string Label
+        {
+            get
+            {
+                return this.label;
+            }
+        }
+
+        #endregion
+
+        #region " Fields "
+
+        private string label;
+
+        #endregion
+
+        #region " Events - INotifyPropertyChanged "
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region " Events "
+
+        #endregion
+
+        #region " Constructure "
+
+        public MainWindowModel()
+        {
+            this.label = "Hello World!";
+
+            //You can change the world...
+
+        }
+
+        #endregion
+
+        #region " Methods - INotifyPropertyChanged "
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ TemplateWpfApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateWpfApp
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowModel MainWindowModel
+        {
+            get
+            {
+                return this.mainWindowModel;
+            }
+        }
+
+        private MainWindowModel mainWindowModel;
+
+        public MainWindowViewModel()
+        {
+            this.mainWindowModel = new MainWindowModel();
+        }
+    }
+}
